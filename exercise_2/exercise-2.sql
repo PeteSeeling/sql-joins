f1b01878e036ff32f815aef0b689a7e33fe0ec87
Pete Seeling <peteseeling@gmail.com>
2022-03-21 16:15:24 -0600

    working on 2

--- exercise_2/exercise-2.sql
+++ exercise_2/exercise-2.sql
@@ -1,2 +1,13 @@
 -- find all rental dates for customers with the first_name Patricia
 -- use INNER JOIN FROM customer onto rental
+SELECT
+    customer.first_name
+FROM
+    customer
+INNER JOIN
+    rental
+on
+rental.rental_date = customer.first_name
+WHERE
+    customer.customer.first_name = 'Patricia'
+
