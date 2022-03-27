c9441397481783e720622c7ac4700eda9f389c01
Pete Seeling <peteseeling@gmail.com>
2022-03-27 13:43:32 -0600

    9 passing

--- exercise_9/exercise-9.sql
+++ exercise_9/exercise-9.sql
@@ -1,3 +1,18 @@
 -- find the film title of all inventory items that have never been rented
 -- LEFT JOIN FROM inventory to film and to rental
 -- use WHERE IS NULL
+
+SELECT 
+title
+FROM
+inventory
+LEFT JOIN
+rental
+on
+rental.inventory_id = inventory.inventory_id
+LEFT JOIN
+film
+on
+film.film_id = inventory.film_id
+WHERE
+rental.rental_id IS NULL
